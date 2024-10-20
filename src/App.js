148584fd345a60d984a42490b05493db25854372
Kavya <kavya01092003@gmail.com>
2024-10-20 19:33:18 +0530

    react codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  let [counter,setCounter]= useState(15);
+ 
+   //let counter=15;
+   const addValue=()=>{
+     console.log("clicked",)
+    // counter=counter+1;
+    setCounter(counter+1);
+   }
+ 
+   const removeValue=()=>{
+     setCounter(counter-1);
+   }
+ 
+   return (
+     <>
+       <h1>React</h1>
+       <h2>Counter Value:{counter}</h2>
+       <button
+        onClick={addValue}
+        >Add value</button>
+       <br/>
+       <button
+       onClick={removeValue}
+       >Remove value</button>
+     </>
+   )
+ }
 
 export default App;
